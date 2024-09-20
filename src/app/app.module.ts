b81b0679434085e00b51e3c3cd59cd3e4e4d2c4b
Farntastic <142087773+Farntastic@users.noreply.github.com>
2024-09-20 12:05:27 +0700

    component artist

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ArtistComponent } from './components/artist/artist.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
